05988a0d9d58f5925565d087f9de96f3272d28ad
Solsospec <158485736+Solsospec@users.noreply.github.com>
2025-06-03 19:08:14 +0100

    Update t.py

--- t.py
+++ t.py
@@ -1,41 +1,52 @@
 import requests
 import datetime
 import pytz
+import math
 
 
 def main():
 	count = 1
 	counter = 1
 	thresh = 50.00001
-	desire = 249.99999
-	multiplier = 0.99945
+	multiplier = 0.99925
+	
+	sell = 162.21
+	buy = 161.39
+	step = 0.00005
+	
+	calc = (1 / math.pow(0.999, 2)) * (math.sqrt(sell / buy))
+	
+	fees = round(calc if round(calc / step) * step == calc else math.ceil(calc / step) * step, 5)
+
+	coin = "SOL"
 	
 	while True:
 		state = "   YES!" if count > 1 else ""
 		print(f"Running... {counter}{state}")
 
 		try:
-			c_1 = coinbase(multiplier)
-			q = qdx()
-			c_2 = coinbase(multiplier)
+			c_1 = coinbase(multiplier, coin)
+			q = qdx(coin)
+			c_2 = coinbase(multiplier, coin)
 			c = c_1 - c_2
 			diff = c_2 - q
+			c_q = c_2 / q
 		
-			if c < thresh and diff > desire:
+			if c < thresh and c_q > fees:
+				period = rec()
 				if count == 1:
-					fill(count, diff, rec())
+					fill(count, diff, period, c_2, c_q)
 										
 				else:
 					with open("difference.txt", "r") as file:
 						lines = file.readlines()
-					d = float(lines[1])
+					cq = float(lines[4].split(" ")[-1])
 							
-					if d > diff:
+					if cq > c_q:
 						pass
 								
 					else:
-						period = rec()
-						fill(count, diff, period)
+						fill(count, diff, period, c_2, c_q)
 
 				count += 1
 		
@@ -46,23 +57,23 @@ def main():
 		counter += 1
 
 
-def coinbase(multiplier):
-	url = "https://api.coinbase.com/v2/prices/BTC-USDC/spot"
+def coinbase(multiplier, coin):
+	url = f"https://api.coinbase.com/v2/prices/{coin}-USDC/spot"
 	response = requests.get(url, timeout=0.3)
 	data = response.json()["data"]["amount"]
 	price = float(data) * multiplier
 	return price
 
 
-def qdx():
-	url = "https://www.quidax.com/api/v1/markets/btcusdt/order_book"
+def qdx(coin):
+	url = f"https://www.quidax.com/api/v1/markets/{coin.lower()}usdt/order_book"
 	response = requests.get(url, timeout=0.5)
 	order = response.json()["data"]["asks"][0]["price"]
 	return float(order)
 	
 
-def fill(count, diff, period):
-	alert = f"{count}\n{diff}\n{period}"
+def fill(count, diff, period, cbs, ratio):
+	alert = f"Occurrences: {count}\nPrice difference: {diff}\nPeriod: {period}\nCoinbase price: {cbs}\nRatio: {ratio}"
 	with open("difference.txt", "w") as file:
 		file.write(alert)
 	bot(alert)
